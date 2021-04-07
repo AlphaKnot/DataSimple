18decbb53b195ff67b881f866f2072f074aa5529
tsuyuuu <57511975+tsuyuuu@users.noreply.github.com>
2021-04-07 19:27:18 -0400

    javadocs done

--- src/DataProcessorException.java
+++ src/DataProcessorException.java
@@ -1,7 +1,7 @@
 /***
  * Exception class for certain cases within dataprocessing.
+ * @author Ishaan Kumar
  */
-
 public class DataProcessorException extends Exception {
     public DataProcessorException(String message){
         super(message);
